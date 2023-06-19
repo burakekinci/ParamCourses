0d104495d444cac3522ce2ad7fda599ba0777b27
burakekinci <burakekinci76@yandex.com>
2023-06-19 23:30:18 +0300

    account controller crud added

--- ParamApi/ParamApi/Controllers/AccountController.cs
+++ ParamApi/ParamApi/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ParamApi.Data.Model;
 using ParamApi.Data.UnitOfWork.Abstract;
 
 namespace ParamApi.Controllers
@@ -20,5 +21,84 @@ namespace ParamApi.Controllers
             var accounts = await unitOfWork.AccountRepository.GetAllAsync();
             return Ok(accounts);
         }
+
+        [HttpGet("GetByIdQuery")]
+        public async Task<IActionResult> GetById([FromQuery] int id)
+        {
+            var account = await unitOfWork.AccountRepository.GetByIdAsync(id);
+
+            if (account is null)
+            {
+                return NotFound();
+            }
+            return Ok(account);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            account.CreatedAt = DateTime.UtcNow;
+            account.CreatedBy = "SystemUser";
+
+            await unitOfWork.AccountRepository.InsertAsync(account);
+            await unitOfWork.CompleteAsync();
+
+            return CreatedAtAction("GetById", new { account.Id }, account);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Account account)
+        {
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+
+            var item = await unitOfWork.AccountRepository.GetByIdAsync(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            item.Name = account.Name;
+            item.UserName = account.UserName;
+            item.Email = account.Email;
+
+            item.LastActivity = DateTime.UtcNow;
+
+            unitOfWork.AccountRepository.Update(item);
+            await unitOfWork.CompleteAsync();
+
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+
+            var item = await unitOfWork.AccountRepository.GetByIdAsync(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            unitOfWork.AccountRepository.RemoveAsync(item);
+            await unitOfWork.CompleteAsync();
+
+            return NoContent();
+        }
+
     }
 }
--- ParamApi/ParamApi/Extension/StartUpDIExtension.cs
+++ ParamApi/ParamApi/Extension/StartUpDIExtension.cs
@@ -9,9 +9,9 @@ namespace ParamApi.Extension
         {
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            //services.AddScoped<ScopeService>();
-            //services.AddSingleton<SingletonService>();
-            //services.AddTransient<TransientService>();
+            //services.AddScoped<ScopeService>();           //Request süresince bir kere oluşturulur, request bitince dispose olur
+            //services.AddSingleton<SingletonService>();    //API yeniden başlayana kadar servis class'ından yalnızca bir tane bulunur
+            //services.AddTransient<TransientService>();    //Requestin her actionında Instance yeniden oluşturulur
 
 
         }
